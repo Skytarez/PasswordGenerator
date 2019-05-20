d1554169c4105d9a509a7c2b374b8e1c868f3041
Skytarez <39680386+Skytarez@users.noreply.github.com>
2019-05-20 22:32:06 +0200

    Pushing words in a txt file + cmd args reader + re-organized variables and default value

--- main.py
+++ main.py
@@ -1,50 +1,56 @@
+import sys
+
+
 latin_key = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz' \
-              '123456789!?&#~={}[]()+-*,.;:_°|@§`%£$¤¨µ^<>²'
+            '123456789!?&#~={}[]()+-*,.;:_°|@§`%£$¤¨µ^<>²'
 
 def get_string_from_code(coded_list, key):
     """
-    @coded_list : list comportant les indices a prendre de key
-    @key : string comportant tous les caracteres utilises
+    coded_list : list comportant les indices a prendre de key
+    key : string comportant tous les caracteres utilises
     """
     decoded = ''
-    for char in coded_list:
-        decoded += key[char]
+    for coded_char in coded_list:
+        decoded += key[coded_char]
     return decoded
 
-def generate_password(length, key=latin_key):
+def write_password(str_length='1', str_per_line='1', filename='password.txt',
+                   key=latin_key):
     """
-    @length : Nombre de caracteres
-    @key : string comportant tous les caracteres utilises
+    length : Nombre de caracteres
+    key : string comportant tous les caracteres utilises
     """
 
-    # Initialisation
+    per_line = int(str_per_line)
+    length = int(str_length)
     password = []
     pos = 0
     max_char_code = len(key) - 1
     coded_list = [0] * length
-    split = '\n'
+    actual_line_length = 0
+    number_of_password = 0
 
-    # Creation du mdp
-    while True:
-        if coded_list[pos] + 1 <= max_char_code:
-            coded_list[pos] += 1
-            password.append(get_string_from_code(coded_list, key))
-        else:
-            pos += 1
-            while pos < length and coded_list[pos] == max_char_code:
-                pos += 1
-            if pos < length:
-                for i in range(pos):
-                    coded_list[i] = 0
+    with open(filename, 'w') as file:
+        while True:
+            file.write(get_string_from_code(coded_list, key) + ' ')
+            number_of_password += 1
+            actual_line_length += 1
+            if actual_line_length == per_line:
+                file.write('\n')
+                actual_line_length = 0
+            if coded_list[pos] + 1 <= max_char_code:
                 coded_list[pos] += 1
-                password.append(get_string_from_code(coded_list, key))
-                pos = 0
             else:
-                break
-    # Retour de CODE
-    return password
+                pos += 1
+                while pos < length and coded_list[pos] == max_char_code:
+                    pos += 1
+                if pos < length:
+                    for i in range(pos):
+                        coded_list[i] = 0
+                    coded_list[pos] += 1
+                    pos = 0
+                else:
+                    break
+    print('%d passwords were written to the file: %s' % (number_of_password, filename))
 
-#-------------[EXECUTION CODE]-----------------------------
-generated = generate_password(4)
-print(generated)
-print(len(generated))
+write_password(*sys.argv[1:])
